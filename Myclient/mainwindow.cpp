affbc0067d87a974d71c07333c1fb48124172ae8
tapkoo <61703251+tapkoo@users.noreply.github.com>
2022-01-30 16:59:41 +0200

    Add files via upload

---
+++ Myclient/mainwindow.cpp
@@ -0,0 +1,92 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+    nextBlockSize = 0;
+}
+
+MainWindow::~MainWindow(){}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    socket->connectToHost("127.0.0.1",5555);
+}
+
+void MainWindow::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_1);
+    out << quint16() << QTime::currentTime() << str;
+    out.device()->seek(0);
+    out << quint16(Data.size() - sizeof(quint16));
+    socket->write(Data);
+    ui->lineEdit->clear();
+}
+
+void MainWindow::slotReadyRead()
+{
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_1);
+    if(in.status()==QDataStream::Ok)
+    {
+
+        for(;;)
+        {
+            if(nextBlockSize == 0)
+            {
+                if(socket->bytesAvailable() < 2)
+                {
+                    break;
+                }
+                in >> nextBlockSize;
+            }
+            if(socket->bytesAvailable() < nextBlockSize)
+            {
+                break;
+            }
+
+            QString str;
+            QTime time;
+            in >> time >> str;
+            nextBlockSize = 0;
+            ui->textBrowser->append(time.toString() + ": " + str);
+        }
+    }
+    else
+    {
+        ui->textBrowser->append("read error");
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    SendToServer(ui->lineEdit->text());
+}
+
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    SendToServer(ui->lineEdit->text());
+}
+
+
+void MainWindow::on_lineEdit_2_returnPressed()
+{
+    SendToServer(ui->lineEdit_2->text());
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QMessageBox::information(this, "Information about list room","In this moment available is only one room, another one will be add in the future");
+}
+
